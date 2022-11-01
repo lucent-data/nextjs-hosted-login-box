34c41cba4f973e3d265522840d88526dbf3d15c9
MashaPav <masha.pavlotsky@frontegg.com>
2022-11-01 10:02:06 +0200

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { withFronteggApp } from '@frontegg/nextjs';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default withFronteggApp(CustomApp, 
+  {
+    hostedLoginBox: true, 
+    authOptions: {
+    // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+    }
+});
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { GetServerSideProps } from 'next';
+import { withSSRSession, useAuth } from '@frontegg/nextjs';
+
+export default function MyPage({ products }) {
+  const {user} = useAuth();
+  
+  //baseUrl should be your FRONTEGG_APP_URL from .env.local
+  const baseUrl =  'FRONTEGG_APP_URL'
+  
+  const logout = () => {
+    window.location.href = `${baseUrl}/account/logout`;
+  };
+
+  return (
+    <div>
+      <h1>My Page</h1>
+       {products}
+      <div>
+        <img src={user?.profilePictureUrl} alt={user?.name}/>
+      </div>
+      <div>
+        <span>Logged in as: {user?.name}</span>
+      </div>
+            <div>
+        <button onClick={logout}>Log out</button>
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = withSSRSession(
+  async (context, session) => {
+//     const { data } = await fetch('{external}/product', {
+//      headers: {
+//        Authorization: 'bearer ' + session.accessToken,
+//      },
+//    });
+    return { props: { } };
+  }
+);
