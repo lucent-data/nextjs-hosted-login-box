f4f23fd38dcdd4f51726838d17e7fc9f16dec4fd
Vedran Vukasovic <vedran.vukasovic@lucentdata.io>
2023-10-31 16:48:23 +0100

    Update withFronteggApp options.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,14 @@ function CustomApp({ Component, pageProps }: AppProps) {
 
 export default withFronteggApp(CustomApp, {
   hostedLoginBox: true,
+  contextOptions: {
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    baseUrl: process.env.FRONTEGG_BASE_URL,
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    clientId: process.env.FRONTEGG_CLIENT_ID,
+  },
+  lazyLoadAdminPortal: true,
   authOptions: {
-    // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+    keepSessionAlive: true,
   },
 });
